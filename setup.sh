5817e250dd21cad6796d86117f368bb740e43e96
dongdong <tjehdghks4356@gmail.com>
2023-12-06 21:15:29 +0100

    Make new conf file for maing local.conf

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@
 DIR="$(pwd)/build"
 MACHINE="raspberrypi4-64"
 LOCAL_CONF="${DIR}/conf/local.conf"
+ADD_LOCAL_CONF="../meta-infotainment/tools/addlocal.conf"
 
 # Initialize Poky environment
 echo "Initializing Poky Environment..."
@@ -21,57 +22,18 @@ bitbake-layers add-layer ../meta-openembedded/meta-python
 bitbake-layers add-layer ../meta-qt5
 bitbake-layers add-layer ../meta-infotainment
 
-# Update local.conf with given settings
-echo "Updating local.conf..."
-cat <<EOF >> $LOCAL_CONF
-# Setting for camera in wayland compositor
-PACKAGECONFIG:remove:pn-qtwayland = "xcomposite-glx"
-VIDEO_CAMERA = "1"
+# Check if addlocal.conf exists
+if [ -f "$ADD_LOCAL_CONF" ]; then
+    # Append addlocal.conf to local.conf
+    echo "Appending addlocal.conf to local.conf..."
+    cat "$ADD_LOCAL_CONF" >> "$LOCAL_CONF"
+    echo "addlocal.conf has been appended to local.conf"
+else
+    echo "Error: addlocal.conf does not exist."
+fi
 
-# Setting for CAN 2-CH FD
-KERNEL_DEVICETREE:append = " \
-                        overlays/mcp251xfd.dtbo \
-"
+echo "Setup completed!"
 
-# Setting for i2c
-ENABLE_I2C = "1"
-KERNEL_MODULE_AUTOLOAD:rpi += "i2c-dev i2c-bcm2708"
-
-# Add dtbo files for waveshare 7.9inch LCD
-KERNEL_DEVICETREE:append = " \
-                        overlays/vc4-kms-dsi-waveshare-panel.dtbo \
-"
-
-# Install dtbo for waveshare 7.9inch LCD
-RPI_EXTRA_CONFIG += " \n\
-dtoverlay=vc4-kms-dsi-waveshare-panel,7_9_inch \n\
-"
- 
-# Set systemd instead sysV
-DISTRO_FEATURES:append = " systemd"
-DISTRO_FEATURES:remove = "sysvinit"
-VIRTUAL-RUNTIME_init_manager = "systemd"
-VIRTUAL-RUNTIME_initscripts = "systemd-compat-units"
-DISTRO_FEATURES_BACKFIL_CONSIDERED = "sysvinit"
-VIRTUAL-RUNTIME_initscript = "systemd-compat-units"
-
-# Set wayland for Qt
-DISTRO_FEATURES:remove = "x11 vulkan"
-DISTRO_FEATURES:append = " wayland"
-CORE_IMAGE_EXTRA_INSTALL = "wayland"
-
-# Ignore old license error
-LICENSE_FLAGS_ACCEPTED += "synaptics-killswitch"
-
-# Set for qt5 gstreamer
-LICENSE_FLAGS_ACCEPTED = "commercial"
-PACKAGECONFIG:append_pn-qtmultimedia=" gstreamer1.0"
-PACKAGECONFIG:append_pn-gstreamer1.0-plugins-bad = " hls"
-
-# For raspberrypi4
-MACHINE = "raspberrypi4-64"
-
-EOF
 
 echo "local.conf updated!"
 echo "Setup completed!"
